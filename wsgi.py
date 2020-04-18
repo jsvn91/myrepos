171c09648bbcc5281f69035da86029efeef33b6d
Swapnil Jagtap <jsvn91@gmail.com>
2020-04-19 00:04:47 +0530

    added for test

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,5 @@ from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nirla.settings") #nirla is the name of the project
 
 # added comment
+# test added
 application = Cling(get_wsgi_application())
